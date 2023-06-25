4f19ed1e52c179f571944b8a11a62791c8da468a
Omar Al-Bayomi <baiomayomar@gmail.com>
2023-06-25 03:40:54 +0300

    new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Welcome from './componentes/Welcome';
 
 function App() {
   const name = "Omar"
+
+  function greating(name){
+    return `Hi, ${name}`
+  }
+
   return (
+    <>
     <div className="App">
       <h1>Hello World! {name}</h1>
+      <h2>{greating("Omar Bayoumi")}</h2>
+
+      <Welcome name="Omar"/>
     </div>
+    </>
   );
 }
 
